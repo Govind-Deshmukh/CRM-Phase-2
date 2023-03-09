c32e23e8215b4587ccf9428886f436f67c15a33a
govindd201 <govind.deshmukh@testaing.com>
2023-03-09 16:40:38 +0530

    appwrite signup done and login is partially done

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import Login from './components/auth/login';
+import Register from './components/auth/register';
+import Home from './components/landing/index';
+import Forgot from './components/auth/forgot';
+import { BrowserRouter as Router, Routes , Route } from 'react-router-dom';
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+     <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path='/forgot' element={<Forgot />} />
+          {/* <Route path="*" element={<h1>404 Not Found</h1>} /> */}
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
