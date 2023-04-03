817a82b19a77ddd39b4aca77b26c8a6ac04d9e63
govindd201 <govind.deshmukh@testaing.com>
2023-04-03 16:58:23 +0530

    google auth and login auth is added

--- src/components/dashboard/home.js
+++ src/components/dashboard/home.js
@@ -1,15 +1,51 @@
 import React, { useEffect, useState } from "react";
-import Navbar from "./navbar";
-import { useNavigate } from "react-router-dom";
 import account from "../services/appwriteConfig";
+import { useNavigate } from "react-router";
 import swal from "sweetalert";
-
-export default function Home() {
+import Navbar from "./navbar";
+const Home = () => {
   const navigate = useNavigate();
+  const [userDetails, setUserDetails] = useState();
 
-  return (
-    <div>
-      <Navbar />
-    </div>
-  );
-}
+  const fetchUser = async () => {
+    try {
+      const data = await account.get();
+      setUserDetails(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchUser();
+  }, [userDetails]);
+
+  if (userDetails) {
+    return (
+      <>
+        <Navbar />
+      </>
+    );
+  } else {
+    return (
+      <div className="d-flex justify-content-center">
+        <div class="card m-5" style={{ width: "18rem" }}>
+          <div class="card-body">
+            <h5 class="card-title">You are not logged in</h5>
+            <h6 class="card-subtitle mb-2 text-muted">
+              Please login first to continue . . .
+            </h6>
+            <button
+              className="btn btn-primary text-center"
+              onClick={() => navigate("/login")}
+            >
+              Login
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+};
+
+export default Home;
